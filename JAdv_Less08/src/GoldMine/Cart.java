81558d2c6da50f2444317735dc56be43df3a386d
poison1512 <artofpoison68@gmail.com>
2021-07-07 17:03:58 +0300

    Lession 8

---
+++ JAdv_Less08/src/GoldMine/Cart.java
@@ -0,0 +1,16 @@
+package GoldMine;
+
+public class Cart {
+
+	public int cartCapacity;
+
+	public Cart(int cartCapacity) {
+		this.cartCapacity = cartCapacity;
+	}
+
+	public int getCartCapacity() {
+		return cartCapacity;
+	}
+	
+	
+}
